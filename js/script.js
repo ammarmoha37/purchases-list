14b2f6601c8e928bdcd7c518da7342ce359f4ea2
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-05-27 01:42:07 +0300

    adding firebase DB

---
+++ js/script.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://purchaseslist-ef8a1-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputEl = document.getElementById("input-field")
+const addBtn = document.getElementById("add-button")
+
+addBtn.addEventListener("click", function() {
+  let inputValue = inputEl.value
+
+  push(shoppingListInDB, inputValue)
+  console.log(inputValue)
+})
