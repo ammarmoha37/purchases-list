a935c0b2e34e52b6fb9f2e3c47f260ee54a1261f
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2023-05-28 02:44:35 +0300

    refactoring code and add button

--- js/script.js
+++ js/script.js
@@ -13,54 +13,94 @@ const inputEl = document.getElementById("input-field")
 const addBtn = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+addBtn.disabled = true        // disabling add button
 
-addBtn.addEventListener("click", function() {
-  let inputValue = inputEl.value
+// add item to database when clicked
+addBtn.addEventListener("click", addingItem)
 
-  push(shoppingListInDB, inputValue)
+// Listen to shopping list in database
+onValue(shoppingListInDB, getItemsFromDB) 
 
-  cleerInput()
-})
+// adding item to database
+function addingItem() {
+  let inputValue = inputEl.value.trim()
 
-onValue(shoppingListInDB, function(snapshot) {
-  if (snapshot.exists()) {
-    let shoppingListArr = Object.entries(snapshot.val())
+  if (inputValue !== "") {              //checking if the input field is not empty
+    push(shoppingListInDB, inputValue)    // push item to database
+    clearInput()                          // clearing input field
+  }
+}
+
+// getting item form database using sanpshot
+function getItemsFromDB(snapshot) {
+  if (snapshot.exists()) {             // checking if there is items in database
+    let shoppingListArr = Object.entries(snapshot.val())  // getting snapshot value and converting it to array
     
-    clearShoppingList()
+    clearShoppingList()     
 
-    for (let i = 0; i < shoppingListArr.length; i++) {
+    for (let i = 0; i < shoppingListArr.length; i++) {  // iterating trought the item of the snapshot array
       let currentItem = shoppingListArr[i]
       
-      appendShoppingItems(currentItem)
+      appendShoppingItems(currentItem)          // displaying items
     }
   } else {
-    let text = document.createElement("p")
-    text.textContent = "No shopping list"
-    clearShoppingList()
-    shoppingListEl.append(text)
+    let text = document.createElement("p")    // creating paragraph
+    text.textContent = "No shopping list"     
+    clearShoppingList()             
+    shoppingListEl.append(text)               // displaying text
   }
-})
+  addBtn.disabled = true                    // disabling add button
+}
 
+// clearing shopping list
 function clearShoppingList() {
   shoppingListEl.innerHTML = ""
 }
 
-function cleerInput() {
+// clearing input field
+function clearInput() {
   inputEl.value = ""
 }
 
+// appending item to shopping list
 function appendShoppingItems(item) {
-  let newItemEl = document.createElement("li")
+  let newItemEl = document.createElement("li")    //creating new item element
 
-  let itemID = item[0]
-  let itemValue = item[1]
+  let itemID = item[0]                      // setting items' ID from snapshot array 
+  let itemValue = item[1]                   // setting items' value from snapshot array
 
-  newItemEl.textContent = itemValue
+  newItemEl.textContent = itemValue         // adding the value of item to the new item element
 
-  newItemEl.addEventListener("click", function() {
-    let LocationOfItem = ref(database, `shoppingList/${itemID}`)
-    remove(LocationOfItem)
+  // removing item form list
+  newItemEl.addEventListener("click", function() {    
+    let LocationOfItem = ref(database, `shoppingList/${itemID}`)    // getting the exact location of the item
+    remove(LocationOfItem)      // removing item form database
   })
 
-  shoppingListEl.append(newItemEl)
+  shoppingListEl.append(newItemEl)      // displaying the new item to the list
+}
+
+// Call the checkInput function when input value changes
+inputEl.addEventListener("input", checkInput)
+
+// Listen for keydown event on the input field
+inputEl.addEventListener("keydown", handleKeyPress)
+
+// Listen for click event on the add button to check if the input field is empty or not
+function checkInput() {
+  let inputValue = inputEl.value.trim()
+
+  if (inputValue !== "") {
+    addBtn.disabled = false   // Enable the button   
+  } else {
+    addBtn.disabled = true    // Disable the button
+  }
+}
+
+// Listen for keydown event on the input field 
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault()      // Prevent form submission
+    addBtn.click()            // Trigger button click
+  }
 }
